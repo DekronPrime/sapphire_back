f139c982f2a31b98f094f99bd3b14c310749fcbc
Dekron <forpresentelite@gmail.com>
2025-06-21 21:27:19 +0300

    updated cors policy

--- src/main/java/com/bs/sapphire/configs/WebConfig.java
+++ src/main/java/com/bs/sapphire/configs/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://sapphire-front.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
